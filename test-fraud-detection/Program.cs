2f1e63a4e81c42c825c7e099e0250251c44ed5c3
Rodrigo Fernández Díaz <rodrigo.diaz@toptal.com>
2024-06-02 17:24:19 +0200

    Basic working implementation

--- test-fraud-detection/Program.cs
+++ test-fraud-detection/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace FraudDetection
 {
-  class Order
+  public class Order
   {
     public required int OrderId { get; set; }
     public required int DealId { get; set; }
@@ -10,6 +10,7 @@
     public required string State { get; set; }
     public required string ZipCode { get; set; }
     public required string CreditCard { get; set; }
+    public string FullAddress { get; private set; }
 
     public static Order Parse(string line)
     {
@@ -25,7 +26,8 @@
         City = parts[4],
         State = parts[5],
         ZipCode = parts[6],
-        CreditCard = parts[7]
+        CreditCard = parts[7],
+        FullAddress = $"{parts[3]}, {parts[4]}, {parts[5]}, {parts[6]}"
       };
     }
   }
@@ -46,13 +48,84 @@
         return;
       }
 
+      var detector = new OrderFraudDetector();
+
       // Read records
-      var records = new List<Order>();
+      var orders = new List<Order>();
       for (int i = 0; i < n; i++)
       {
         var line = Console.ReadLine();
         var order = Order.Parse(line);
-        records.Add(order);
+        orders.Add(order);
+      }
+
+      // Validate records
+      detector.ValidateOrders(orders);
+
+      // Print fraudulent orders as a comma-separated list of order IDs
+      Console.WriteLine(string.Join(",", detector.FraudulentOrders.Select(o => o.OrderId)));
+    }
+  }
+
+  public class OrderFraudDetector
+  {
+    public HashSet<Order> FraudulentOrders { get; private set; }
+
+    public OrderFraudDetector()
+    {
+      FraudulentOrders = new HashSet<Order>();
+    }
+
+    // Keep an index of orders by deal ID and email
+    Dictionary<int, Dictionary<string, Order>> dealsByEmail = new Dictionary<int, Dictionary<string, Order>>();
+    // Keep an index of orders by deal ID and full address
+    Dictionary<int, Dictionary<string, Order>> dealsByAddresses = new Dictionary<int, Dictionary<string, Order>>();
+
+    public void ValidateOrders(List<Order> orders)
+    {
+      foreach (var order in orders)
+      {
+        if (dealsByEmail.ContainsKey(order.DealId))
+        {
+          if (dealsByEmail[order.DealId].ContainsKey(order.Email))
+          {
+            var existingOrder = dealsByEmail[order.DealId][order.Email];
+            if (existingOrder.CreditCard != order.CreditCard)
+            {
+              FraudulentOrders.Add(existingOrder);
+              FraudulentOrders.Add(order);
+            }
+          }
+          else
+          {
+            dealsByEmail[order.DealId].Add(order.Email, order);
+          }
+        }
+        else
+        {
+          dealsByEmail.Add(order.DealId, new Dictionary<string, Order> { { order.Email, order } });
+        }
+
+        if (dealsByAddresses.ContainsKey(order.DealId))
+        {
+          if (dealsByAddresses[order.DealId].ContainsKey(order.FullAddress))
+          {
+            var existingOrder = dealsByAddresses[order.DealId][order.FullAddress];
+            if (existingOrder.CreditCard != order.CreditCard)
+            {
+              FraudulentOrders.Add(existingOrder);
+              FraudulentOrders.Add(order);
+            }
+          }
+          else
+          {
+            dealsByAddresses[order.DealId].Add(order.FullAddress, order);
+          }
+        }
+        else
+        {
+          dealsByAddresses.Add(order.DealId, new Dictionary<string, Order> { { order.FullAddress, order } });
+        }
       }
     }
   }
